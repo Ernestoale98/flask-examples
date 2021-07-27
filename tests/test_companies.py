3d8857d2ae8e78a33c5d92c1c96711cc6c5d40fe
Ernesto Munoz <netocontreras1998@outlook.com>
2021-07-27 17:41:40 -0500

    Create companies tests of query operators

--- tests/test_companies.py
+++ tests/test_companies.py
@@ -27,6 +27,25 @@ def test_sort_companies_with_pymongo(client,db):
     assert companies[0]['name'] == 'zyntroPICS'
 
 
+@pytest.mark.usefixtures('db')
+def test_query_operators_with_pymongo(client,db):
+    companies = db.companies.count_documents({
+        "number_of_employees":{
+            "$gte":50
+        }
+    })
+    assert companies == 904
+
+    companies = db.companies.count_documents({
+        "category_code":{"$not":{"$eq":"web"}}
+    })
+    assert companies == 7553
+
+    companies = db.companies.count_documents({
+        "acquisitions":{"$size":10}
+    })
+    assert companies == 4
+
 """MongoEngine"""
 
 
@@ -45,3 +64,15 @@ def test_sort_companoes_with_mongoengine(client,connection):
     companies = list(Company.objects().order_by('-name').limit(5))
     assert len(companies) == 5
     assert companies[0]['name'] == 'zyntroPICS'
+
+
+@pytest.mark.usefixtures('connection')
+def test_query_operators_with_mongoengine(client,connection):
+    companies = Company.objects(number_of_employees__gte=50).count()
+    assert companies == 904
+
+    companies = Company.objects(category_code__not__exact='web').count()
+    assert companies == 7553
+
+    companies = Company.objects(acquisitions__size=10).count()
+    assert companies == 4
